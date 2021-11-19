6eff81c905fa9a799787f00c4f1ad261f60e7cf9
ninja <kirillsurov0@gmail.com>
2021-11-20 05:07:08 +0900

    update(docs): rename htmlwebpackplugin option plugin

--- vue.config.js
+++ vue.config.js
@@ -5,4 +5,13 @@ module.exports = {
     devServer: {
         port: 9000
     },
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].title = 'Vue Style';
+
+                return args;
+            });
+    }
 };
